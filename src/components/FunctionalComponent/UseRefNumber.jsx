87f84baefa069b4789486abdc610b51d9c471cc1
sudharshan1011 <sudharshans.22aim@kongu.edu>
2025-01-07 11:00:14 +0530

    8th day

--- src/components/FunctionalComponent/UseRefNumber.jsx
+++ src/components/FunctionalComponent/UseRefNumber.jsx
@@ -1,19 +1,17 @@
-// import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 
-// var UseRefNumber = () =>{
-//     const [name, setName] = useState('')
-//     const renderCount = useRef(0)
-
-//     useEffect(() =>{
-//         renderCount.current = renderCount.current + 1;
-//     })
-
-//     return(
-//         <section>
-//             <input value={name} onChange={e => setName(e.target.value)}/>
-//             <div>College name is {name}</div>
-//             <div>I rendered {renderCount.current}</div>
-//         </section>
-//     )
-// }
-// export default UseRefNumber;
+var UseRefNumber = () =>{
+const [name, setName] = useState('')
+const renderCount = useRef(0)
+useEffect(() =>{
+renderCount.current = renderCount.current + 1;
+ })
+return(
+         <section>
+             <input value={name} onChange={e => setName(e.target.value)}/>
+             <div>College name is {name}</div>
+             <div>I rendered {renderCount.current}</div>
+         </section>
+     )
+ }
+ export default UseRefNumber;
